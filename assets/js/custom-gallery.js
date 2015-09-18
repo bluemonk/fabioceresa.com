b4cbf1b61bab74ffa8c2dc3f2a5ae38382db2859
bluemonk <ceresa@gmail.com>
2015-09-18 19:14:05 +0200

    add galleries to portfolio section

--- assets/js/custom-gallery.js
+++ assets/js/custom-gallery.js
@@ -18,6 +18,263 @@ $('#contact-picture').on('click', function(event) {
   });
 });
 
+$('#puritani').on('click', function(event) {
+  event.preventDefault();
+  blueimp.Gallery([
+    'assets/images/puritani/01.jpg',
+    'assets/images/puritani/02.jpg',
+    'assets/images/puritani/03.jpg',
+    'assets/images/puritani/04.jpg',
+    'assets/images/puritani/05.jpg',
+    'assets/images/puritani/06.jpg',
+    'assets/images/puritani/07.jpg',
+    'assets/images/puritani/08.jpg',
+    'assets/images/puritani/09.jpg',
+    'assets/images/puritani/10.jpg',
+    'assets/images/puritani/11.jpg',
+    'assets/images/puritani/12.jpg',
+    'assets/images/puritani/13.jpg',
+    'assets/images/puritani/14.jpg',
+    'assets/images/puritani/15.jpg',
+    'assets/images/puritani/16.jpg',
+    'assets/images/puritani/17.jpg',
+    'assets/images/puritani/18.jpg',
+    'assets/images/puritani/19.jpg',
+    'assets/images/puritani/20.jpg',
+    'assets/images/puritani/21.jpg',
+    'assets/images/puritani/22.jpg',
+    'assets/images/puritani/23.jpg',
+    'assets/images/puritani/24.jpg',
+    'assets/images/puritani/25.jpg',
+    'assets/images/puritani/26.jpg',
+    'assets/images/puritani/27.jpg',
+    'assets/images/puritani/28.jpg',
+    'assets/images/puritani/29.jpg',
+    'assets/images/puritani/30.jpg',
+    'assets/images/puritani/31.jpg',
+    'assets/images/puritani/32.jpg',
+    'assets/images/puritani/33.jpg',
+    'assets/images/puritani/34.jpg',
+    'assets/images/puritani/35.jpg'
+  ], {
+    fullScreen: true,
+    useBootstrapModal: false
+  });
+});
+
+$('#butt2015').on('click', function(event) {
+  event.preventDefault();
+  blueimp.Gallery([
+    'assets/images/butt2015/01.jpg',
+    'assets/images/butt2015/02.jpg',
+    'assets/images/butt2015/03.jpg',
+    'assets/images/butt2015/04.jpg',
+    'assets/images/butt2015/05.jpg',
+    'assets/images/butt2015/06.jpg',
+    'assets/images/butt2015/07.jpg',
+    'assets/images/butt2015/08.jpg',
+    'assets/images/butt2015/09.jpg',
+    'assets/images/butt2015/10.jpg',
+    'assets/images/butt2015/11.jpg',
+    'assets/images/butt2015/12.jpg',
+    'assets/images/butt2015/13.jpg',
+    'assets/images/butt2015/14.jpg',
+    'assets/images/butt2015/15.jpg',
+    'assets/images/butt2015/16.jpg',
+    'assets/images/butt2015/17.jpg',
+    'assets/images/butt2015/18.jpg',
+    'assets/images/butt2015/19.jpg',
+    'assets/images/butt2015/20.jpg',
+    'assets/images/butt2015/21.jpg',
+    'assets/images/butt2015/22.jpg',
+    'assets/images/butt2015/23.jpg',
+    'assets/images/butt2015/24.jpg'
+  ], {
+    fullScreen: true,
+    useBootstrapModal: false
+  });
+});
+
+$('#trittico').on('click', function(event) {
+  event.preventDefault();
+  blueimp.Gallery([
+    'assets/images/trittico/01.jpg',
+    'assets/images/trittico/02.jpg',
+    'assets/images/trittico/03.jpg',
+    'assets/images/trittico/04.jpg',
+    'assets/images/trittico/05.jpg',
+    'assets/images/trittico/06.jpg',
+    'assets/images/trittico/07.jpg',
+    'assets/images/trittico/08.jpg',
+    'assets/images/trittico/09.jpg',
+    'assets/images/trittico/10.jpg',
+    'assets/images/trittico/11.jpg',
+    'assets/images/trittico/12.jpg',
+    'assets/images/trittico/13.jpg',
+    'assets/images/trittico/14.jpg',
+    'assets/images/trittico/15.jpg',
+    'assets/images/trittico/16.jpg',
+    'assets/images/trittico/17.jpg'
+  ], {
+    fullScreen: true,
+    useBootstrapModal: false
+  });
+});
+
+$('#giovanna').on('click', function(event) {
+  event.preventDefault();
+  blueimp.Gallery([
+    'assets/images/giovanna/01.jpg',
+    'assets/images/giovanna/02.jpg',
+    'assets/images/giovanna/03.jpg',
+    'assets/images/giovanna/04.jpg',
+    'assets/images/giovanna/05.jpg',
+    'assets/images/giovanna/06.jpg',
+    'assets/images/giovanna/07.jpg',
+    'assets/images/giovanna/08.jpg',
+    'assets/images/giovanna/09.jpg',
+    'assets/images/giovanna/10.jpg',
+    'assets/images/giovanna/11.jpg',
+    'assets/images/giovanna/12.jpg',
+    'assets/images/giovanna/13.jpg',
+    'assets/images/giovanna/14.jpg',
+    'assets/images/giovanna/15.jpg',
+    'assets/images/giovanna/16.jpg',
+    'assets/images/giovanna/17.jpg',
+    'assets/images/giovanna/18.jpg',
+    'assets/images/giovanna/19.jpg'
+  ], {
+    fullScreen: true,
+    useBootstrapModal: false
+  });
+});
+
+$('#butt2010').on('click', function(event) {
+  event.preventDefault();
+  blueimp.Gallery([
+    'assets/images/butt2010/0001.jpg',
+    'assets/images/butt2010/0002.jpg',
+    'assets/images/butt2010/0003.jpg',
+    'assets/images/butt2010/0004.jpg',
+    'assets/images/butt2010/0005.jpg',
+    'assets/images/butt2010/0006.jpg',
+    'assets/images/butt2010/0007.jpg',
+    'assets/images/butt2010/0008.jpg',
+    'assets/images/butt2010/0009.jpg',
+    'assets/images/butt2010/0010.jpg',
+    'assets/images/butt2010/0011.jpg',
+    'assets/images/butt2010/0012.jpg',
+    'assets/images/butt2010/0013.jpg'
+  ], {
+    fullScreen: true,
+    useBootstrapModal: false
+  });
+});
+
+$('#tosca').on('click', function(event) {
+  event.preventDefault();
+  blueimp.Gallery([
+    'assets/images/tosca/01.jpg',
+    'assets/images/tosca/02.jpg',
+    'assets/images/tosca/03.jpg',
+    'assets/images/tosca/04.jpg',
+    'assets/images/tosca/05.jpg',
+    'assets/images/tosca/06.jpg',
+    'assets/images/tosca/07.jpg',
+    'assets/images/tosca/08.jpg',
+    'assets/images/tosca/09.jpg',
+    'assets/images/tosca/10.jpg',
+    'assets/images/tosca/11.jpg',
+    'assets/images/tosca/12.jpg',
+    'assets/images/tosca/13.jpg',
+    'assets/images/tosca/14.jpg',
+    'assets/images/tosca/15.jpg',
+    'assets/images/tosca/16.jpg',
+    'assets/images/tosca/17.jpg',
+    'assets/images/tosca/18.jpg',
+    'assets/images/tosca/19.jpg',
+    'assets/images/tosca/20.jpg'
+  ], {
+    fullScreen: true,
+    useBootstrapModal: false
+  });
+});
+
+$('#orfeo').on('click', function(event) {
+  event.preventDefault();
+  blueimp.Gallery([
+    'assets/images/orfeo/01.jpg',
+    'assets/images/orfeo/02.jpg',
+    'assets/images/orfeo/03.jpg',
+    'assets/images/orfeo/04.jpg',
+    'assets/images/orfeo/05.jpg',
+    'assets/images/orfeo/06.jpg',
+    'assets/images/orfeo/07.jpg',
+    'assets/images/orfeo/08.jpg',
+    'assets/images/orfeo/09.jpg',
+    'assets/images/orfeo/10.jpg',
+    'assets/images/orfeo/11.jpg',
+    'assets/images/orfeo/12.jpg',
+    'assets/images/orfeo/13.jpg',
+    'assets/images/orfeo/14.jpg',
+    'assets/images/orfeo/15.jpg',
+    'assets/images/orfeo/16.jpg',
+    'assets/images/orfeo/17.jpg',
+    'assets/images/orfeo/18.jpg',
+    'assets/images/orfeo/19.jpg',
+    'assets/images/orfeo/20.jpg',
+    'assets/images/orfeo/21.jpg',
+    'assets/images/orfeo/22.jpg',
+    'assets/images/orfeo/23.jpg',
+    'assets/images/orfeo/24.jpg',
+    'assets/images/orfeo/25.jpg',
+    'assets/images/orfeo/26.jpg',
+    'assets/images/orfeo/27.jpg',
+    'assets/images/orfeo/28.jpg',
+    'assets/images/orfeo/29.jpg',
+    'assets/images/orfeo/30.jpg'
+  ], {
+    fullScreen: true,
+    useBootstrapModal: false
+  });
+});
+
+$('#traviata').on('click', function(event) {
+  event.preventDefault();
+  blueimp.Gallery([
+    'assets/images/traviata/01.jpg',
+    'assets/images/traviata/02.jpg',
+    'assets/images/traviata/03.jpg',
+    'assets/images/traviata/04.jpg',
+    'assets/images/traviata/05.jpg',
+    'assets/images/traviata/06.jpg',
+    'assets/images/traviata/07.jpg',
+    'assets/images/traviata/08.jpg',
+    'assets/images/traviata/09.jpg',
+    'assets/images/traviata/10.jpg',
+    'assets/images/traviata/11.jpg',
+    'assets/images/traviata/12.jpg',
+    'assets/images/traviata/13.jpg',
+    'assets/images/traviata/14.jpg',
+    'assets/images/traviata/15.jpg',
+    'assets/images/traviata/16.jpg',
+    'assets/images/traviata/17.jpg',
+    'assets/images/traviata/18.jpg',
+    'assets/images/traviata/19.jpg',
+    'assets/images/traviata/20.jpg',
+    'assets/images/traviata/21.jpg',
+    'assets/images/traviata/22.jpg',
+    'assets/images/traviata/23.jpg',
+    'assets/images/traviata/24.jpg'
+  ], {
+    fullScreen: true,
+    useBootstrapModal: false
+  });
+});
+
+
+
+
 
 
 
